8a98f9e431e287cbb29232f1f366706da8802696
dhrumishah <84569241+dhrumishah@users.noreply.github.com>
2022-07-04 07:41:25 +0530

    Update codewars.js

--- 8 kyu/codewars.js
+++ 8 kyu/codewars.js
@@ -1,5 +1,11 @@
-//Q1.
 //Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 function even_or_odd(number) {
   return number % 2 ? "Odd" : "Even";
+}
+
+//Very simple, given an integer or a floating-point number, find its opposite.
+function opposite(number) {
+  let counter = -1;
+  let oppositeNumber = counter * number;
+  return oppositeNumber;
 }
