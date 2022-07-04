375cb3106a63385bc603988e07a0188c0a964299
dhrumishah <84569241+dhrumishah@users.noreply.github.com>
2022-07-04 21:02:33 +0530

    Update codewars.js

--- 8 kyu/codewars.js
+++ 8 kyu/codewars.js
@@ -9,3 +9,11 @@ function opposite(number) {
   let oppositeNumber = counter * number;
   return oppositeNumber;
 }
+
+// We need a function that can transform a number into a string.
+//What ways of achieving this do you know?
+
+function numberToString(num) {
+  let str = num.toString();
+  return str;
+}
