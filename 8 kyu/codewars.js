efad5a7ad156c23132211e7b80c44209ee0e8e8c
dhrumishah <84569241+dhrumishah@users.noreply.github.com>
2022-07-07 08:06:49 +0530

    Update codewars.js

--- 8 kyu/codewars.js
+++ 8 kyu/codewars.js
@@ -36,3 +36,15 @@ function solution(str){
 function makeNegative(num) {
   return num > 0 ? num * -1 : num;
 }
+
+//Sum of positive
+
+function positiveSum(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      sum += arr[i];
+      }
+  }
+  return sum;
+}
