7cd781c1070f4e45901d35532bbc2fc4413bf2e3
dhrumishah <84569241+dhrumishah@users.noreply.github.com>
2022-07-14 11:15:06 +0530

    Update codewars.js

--- 7 kyu/codewars.js
+++ 7 kyu/codewars.js
@@ -42,3 +42,14 @@ function highAndLow(numbers){
   return arr[arr.length-1] + " " +arr[0]
   
 }
+//Get the Middle Character
+
+function getMiddle(s)
+{
+  const i = Math.floor(s.length / 2);
+  let result = s[i];  
+  if(s.length % 2 === 0 && i > 0) {
+    result = s[i-1] + result;
+  }
+  return result;
+}
