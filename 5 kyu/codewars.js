37ce2c6e3c5efdcb01a12400482b0bc89c994d97
dhrumishah <84569241+dhrumishah@users.noreply.github.com>
2022-07-31 22:30:32 +0530

    Update codewars.js

--- 5 kyu/codewars.js
+++ 5 kyu/codewars.js
@@ -19,3 +19,21 @@ var lastDigit = function(str1, str2){
    return +str2 === 0 ? 1 : Math.pow(+str1.slice(-1), (+str2.slice(-2) % 4 || 4)) % 10
 }
  
+// Moving Zeros To The End
+
+function moveZeros(arr) {
+  let zero = []
+  let others = []
+  let together = []
+  
+    for (let i =0; i <= arr.length-1; i++){
+      if (arr[i] === 0){
+        zero.push(arr[i])
+        }
+      else{ 
+        others.push(arr[i])
+      }
+    }
+  together = others.concat(zero)
+  return together
+}
