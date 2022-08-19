5581267f9ae0b6fe433700e7c99c539c755ee99c
dhrumishah <84569241+dhrumishah@users.noreply.github.com>
2022-08-19 16:31:56 +0530

    Update codewars.js

--- 8 kyu/codewars.js
+++ 8 kyu/codewars.js
@@ -257,3 +257,12 @@ function paperwork(n, m) {
 function invert(array) {
    return array.map(x=>x*-1)
 }
+
+// Opposites Attract
+
+function lovefunc(flower1, flower2){
+  if(flower1%2==0&&flower2%2!==0 || flower2%2==0&&flower1%2!==0 ){
+    return true
+  }else
+    return false
+}
